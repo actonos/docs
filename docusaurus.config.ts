48ae3722f0b5642411ee0b5be436d38b5d220933
Bieber Kieu <kieutrongthien@gmail.com>
2026-08-23 21:45:54 +0700

    chore: update git link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          editUrl: 'https://github.com/actonos/actonos-docs/tree/main/',
+          editUrl: 'https://github.com/actonos/docs/tree/main/',
           lastVersion: '0.1',
           versions: {
             current: {
@@ -62,7 +62,7 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          editUrl: 'https://github.com/actonos/actonos-docs/tree/main/',
+          editUrl: 'https://github.com/actonos/docs/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
